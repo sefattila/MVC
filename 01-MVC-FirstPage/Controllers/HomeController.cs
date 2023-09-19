cd403197e1691c30c2c11980263abbc871e95dbe
sefattila <rq7fans@gmail.com>
2023-09-19 16:45:58 +0300

    Add project files.

---
+++ 01-MVC-FirstPage/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using _01_MVC_FirstPage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_MVC_FirstPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // return Content("Merhaba MVC Dünyası - İlk MVC Kodum");
+
+            Movie movie = new Movie() { Id = 1, Title = "The GodFather", ReleaseDate = new DateTime(1972, 2, 24) };
+
+            return View(movie);
+        }
+
+        public IActionResult Create()
+        {
+            return Content("Oluşturuldu");
+        }
+    }
+}
---
+++ 01-MVC-FirstPage/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_MVC_FirstPage.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
