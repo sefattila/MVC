d0584ef0f9ec90374a1ea7c9772e3661cba5ea78
sefattila <rq7fans@gmail.com>
2023-10-02 16:45:50 +0300

    async

---
+++ 01-AsenkronProgramlama/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _01_AsenkronProgramlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task Bekletme(Wait,WaitAll,WaitAny)
+            /*
+             * Wait: İlgili task tamamlanması bitmeden bir sonraki kod satırı çalıştırılamaz
+             * WaitAll: Parametredeki tüm taskların tamamlanması bitmeden bir sonraki kod çalıştırılamaz
+             * WaitAny: Parametredeki tasklardan herhangi biri tamamlanamadan bir sonraki kod satırı çalıştırılamaz
+             */
+
+            //StartSchool().Wait();
+            //Task.WaitAll(StartSchool(), TeachClass1());
+            //TeachClass2();
+
+            Task.WaitAny(StartSchool(), TeachClass1());
+            TeachClass2();
+
+            Console.Read();
+
+            //Async(içerisinde asenkron işlem yapılacak metodu belirler),
+            //Await(async olarak işaretlenen metotda asenkron çalışacak komutları temsil eder.),
+            //Task(asenkron metodu temsil eder)
+        }
+
+        public static async Task StartSchool()
+        {
+            await Task.Delay(8000);
+            Console.WriteLine("Okul Bitti");
+        }
+
+        public static async Task TeachClass1()
+        {
+            await Task.Delay(3000);
+            Console.WriteLine("12. Sınıflar Dersi Aldı");
+        }
+
+        public static async Task TeachClass2()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("11. Sınıflar Dersi Aldı");
+        }
+
+        #region SenkronProgramlama
+
+        //public static void StartSchool()
+        //{
+        //    Thread.Sleep(8000);
+        //    Console.WriteLine("Okul Başladı");
+        //}
+
+        //public static void TeachClass1()
+        //{
+        //    Thread.Sleep(3000);
+        //    Console.WriteLine("12. Sınıf Eğitime başladı");
+        //}
+
+        //public static void TeachClass2()
+        //{
+        //    Thread.Sleep(2000);
+        //    Console.WriteLine("11. Sınıf Eğitime başladı");
+        //}
+        #endregion
+    }
+}
---
+++ 02-Task/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK asenkron programlamada yapılması gerekn işleri temsil eder. Taskın olayı bir işin tamamlanıp,tamamlanmadığını yani durum bilgisini
+            //söyleyebilir. Eğer işlem bir sonuç verirse task bize bu sonucu bildirir.
+
+            //void-> Task GeriDeğerDöndürenMetot -> Task<TResult>
+
+            //Task oluşturmanın 3 yönetimi vardır
+            //1) Task.FactoryçStartView
+            //2) Task.Instance
+            //3) Task.Run Metodu
+
+
+            #region Task-1
+            //var task =new Task(()=>GetRandomNumber());
+            //task.Start();
+
+            //Console.WriteLine("Program Başladı");
+            //Console.Read();
+            #endregion
+
+            #region Task-2
+            //Task.Run(() => GetRandomNumber());
+
+            //Console.WriteLine("Program Başladı");
+            //Console.Read();
+            #endregion
+
+            #region Task-3
+            //Task.Factory.StartNew(() => GetRandomNumber());
+
+            //Console.WriteLine("Program Başladı");
+            //Console.Read();
+            #endregion
+
+            #region Task-4
+            //Task<int> task=Task.Run(() => GetRandomNumber());
+
+            //Console.WriteLine("Rastgele Sayı: "+task.Result);
+            //Console.WriteLine("Program Başladı");
+            //Console.Read();
+            #endregion
+        }
+        static int GetRandomNumber()
+        {
+            Thread.Sleep(1000);
+            int random=new Random().Next(1,100);
+            return random;
+        }
+    }
+}
