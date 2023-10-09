7da4a45ead8b8a23b20d296be768e01d84758f44
sefattila <rq7fans@gmail.com>
2023-10-09 16:20:38 +0300

    asdasd

--- 18-SOLIDPrensibleri/02-OpenClosed/BadImplementation/Employee.cs
+++ 18-SOLIDPrensibleri/02-OpenClosed/BadImplementation/Employee.cs
@@ -10,7 +10,7 @@ namespace _18_SOLIDPrensibleri._02_OpenClosed.BadImplementation
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string? Description { get; set; }    
         public double DailyWages { get; set; }
     }
 }
