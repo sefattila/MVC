9cfd55eb06530e65c364a8be5dbb958f8e2c0995
sefattila <rq7fans@gmail.com>
2023-10-10 16:31:32 +0300

    0121

---
+++ 19-DesignPatterSingleton/EagerSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_DesignPatterSingleton
+{
+    public class EagerSingleton
+    {
+        public static EagerSingleton instance;
+        private EagerSingleton()
+        {
+
+        }
+
+        public static EagerSingleton GetInstance()
+        {
+            {
+                if (instance == null)
+                    instance = new EagerSingleton();
+                return instance;
+            }
+        }
+    }
+}
---
+++ 19-DesignPatterSingleton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _19_DesignPatterSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EagerSingleton.GetInstance();
+        }
+    }
+}
